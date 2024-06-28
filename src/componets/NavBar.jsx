cb89c1c100ac2b7a0c75c040310078a1d7ca7f3c
Sebattaz <seba.lostar@gmail.com>
2024-06-28 19:17:07 -0400

    Agregar iconos, cambio de color letras

--- src/componets/NavBar.jsx
+++ src/componets/NavBar.jsx
@@ -1,6 +1,8 @@
 import { Navbar, Nav, NavbarCollapse}  from "react-bootstrap"
 import { Link} from "react-router-dom"
-
+import { FcHome } from "react-icons/fc";
+import { FcCustomerSupport } from "react-icons/fc";
+import { GiCakeSlice } from "react-icons/gi";
 
 const NavBar = () => {
   return (
@@ -9,12 +11,12 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav"/>
         <NavbarCollapse id="basic-navbar-nav">
             <Nav className="mx-5">
-                <Nav.Link as={Link} to='/'>Home</Nav.Link>
-                <Nav.Link as={Link} to='/contacto' >Contacto</Nav.Link>
+                <Nav.Link as={Link} to='/' className="text-light" ><FcHome /> Home</Nav.Link>
+                <Nav.Link as={Link} to='/contacto' className="text-light"  ><FcCustomerSupport /> Contacto</Nav.Link>
             </Nav>
         </NavbarCollapse>
-        <Navbar.Brand className="mx-5">
-            Happy Cake
+        <Navbar.Brand className="mx-5 ">
+            <span className="text-light">Happy Cake</span>  <GiCakeSlice />
         </Navbar.Brand>
     </Navbar>
   )
--- src/views/ErrorPage.jsx
+++ src/views/ErrorPage.jsx
@@ -1,10 +1,15 @@
 import { Container} from "react-bootstrap"
+import { Nav } from "react-bootstrap"
+import { Link } from "react-router-dom"
 
 const ErrorPage = () => {
   return (
     <Container className="d-flex flex-column align-items-center mt-5">
       <h1>Pastel no encontrado :C</h1>
       <img src="./src/img/broken.png" alt="Broken" />
+      <Nav>
+          <Nav.Link as={Link} to='/' className="bg-success text-white">HOME</Nav.Link>
+      </Nav>
     </Container>
   )
 }
