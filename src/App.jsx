8ccf406c732404bb2c34e5980d0426d3837bac9b
Sebattaz <seba.lostar@gmail.com>
2024-06-28 17:52:27 -0400

    Agregar Vistas

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,23 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import NavBar from './componets/NavBar'
+import { Container } from 'react-bootstrap'
+import Home from './views/Home'
+import ErrorPage from './views/ErrorPage'
+import Contact from './views/Contact'
 function App() {
   
 
   return (
     <BrowserRouter>
       <NavBar/>
-
+        <Container>
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='*' element={<ErrorPage/>}/>
+            <Route path='/contacto' element={<Contact/>} />
+          </Routes>
+        </Container>
     </BrowserRouter>
   )
 }
